be61f026fea1ebdd0397e16b65bfdda882b36eb0
stfrancoc <stfrancoc@gmail.com>
2026-03-29 19:40:44 -0500

    se da funcionalidad en vr el movetracking

--- src/screens/ViewerScreen.js
+++ src/screens/ViewerScreen.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { View, Button, Text, Dimensions } from 'react-native';
 import ImageStream from '../components/ImageStream';
+import { useEffect, useState } from 'react';
+import { Gyroscope } from 'expo-sensors';
 
 const { width } = Dimensions.get('window');
 
 export default function ViewerScreen({ mode, serverIp, onExit }) {
+    const [rotation, setRotation] = useState({ x: 0, y: 0 });
+    
+    useEffect(() => {
+        Gyroscope.setUpdateInterval(16);
+
+        const sub = Gyroscope.addListener(data => {
+            setRotation(prev => ({
+            x: prev.x + data.y * 2,
+            y: prev.y + data.x * 2
+            }));
+        });
+
+        return () => sub.remove();
+    }, []);
+
+
   if (mode === 'flat') {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' }}>
@@ -24,9 +42,31 @@ export default function ViewerScreen({ mode, serverIp, onExit }) {
         
     
         <View style={{ flex: 1, flexDirection: 'row', backgroundColor: 'black' }}>
-        <ImageStream serverIp={serverIp} style={{ width: width / 2 }} />
-        <ImageStream serverIp={serverIp} style={{ width: width / 2 }} />
-        
+  
+            {[0,1].map((_, i) => (
+                <View
+                key={i}
+                style={{
+                    width: width / 2,
+                    height: '100%',
+                    overflow: 'hidden'
+                }}
+                >
+                <View
+                    style={{
+                    width: '120%',
+                    height: '120%',
+                    transform: [
+                        { translateX: rotation.x },
+                        { translateY: rotation.y }
+                    ]
+                    }}
+                >
+                    <ImageStream serverIp={serverIp} />
+                </View>
+                </View>
+            ))}
+
         </View>
 
         <Button title="Salir" onPress={onExit} />
